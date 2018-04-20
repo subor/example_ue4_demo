52b69d1a042483107694e5bf18064bbb6a9a1f15
PLAYRUYI\Goden <goden.zheng@playruyi.com>
2018-04-20 16:16:39 +0800

    [JP-1934] add code example of input event from sdk

--- Source/RuyiSDKDemo/RuyiSDKDemoCharacter.cpp
+++ Source/RuyiSDKDemo/RuyiSDKDemoCharacter.cpp
@@ -66,7 +66,7 @@ void ARuyiSDKDemoCharacter::BeginPlay()
 	
 	if (FRuyiSDKManager::Instance()->IsSDKReady)
 	{
-		FRuyiSDKManager::Instance()->SDK()->Subscriber->Subscribe("service/inputmanager");
+		FRuyiSDKManager::Instance()->SDK()->Subscriber->Subscribe("service/inputmanager_internal");
 		FRuyiSDKManager::Instance()->SDK()->Subscriber->AddMessageHandler(this, &ARuyiSDKDemoCharacter::InputStateChangeHandler);
 	}
 
@@ -92,7 +92,90 @@ void ARuyiSDKDemoCharacter::Ruyi_StartTest()
 //////////////////////////////////////////////////////////////////////////
 void ARuyiSDKDemoCharacter::InputStateChangeHandler(std::string topic, apache::thrift::TBase* msg)
 {
+	//you can use UE4 engine input system, or you can use input system from sdk.
+	//if if you wanna call the input logic in blueprints but still get the input from Ruyi SDK
+	//We recommand you listener all the input in c++ from Ruyi SDK, then call in blueprints
+
 	//UE_LOG(CommonLog, Log, TEXT("ARuyiSDKDemoCharacter::InputStateChangeHandler !!!"));
+
+	auto idsc = dynamic_cast<Ruyi::SDK::InputManager::InputDeviceStateChanged*>(msg);
+
+	if (idsc == NULL)
+	{
+		return;
+	}
+
+	if (idsc->__isset.header == 1)
+	{
+		//UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler header:%d"), idsc->__isset.header);
+	}
+	if (idsc->__isset.x360 == 1)
+	{
+		int64_t PacketNumber = idsc->x360.PacketNumber;
+		//FString fDeviceId = UTF8_TO_TCHAR(idsc->x360.DeviceId.c_str());
+		int16_t LeftThumbDeadZone = idsc->x360.LeftThumbDeadZone;
+		int16_t RightThumbDeadZone = idsc->x360.RightThumbDeadZone;
+		int32_t Buttons = idsc->x360.Buttons;
+		int8_t LeftTrigger = idsc->x360.LeftTrigger;
+		int8_t RightTrigger = idsc->x360.RightTrigger;
+		int16_t LeftThumbX = idsc->x360.LeftThumbX;
+		int16_t LeftThumbY = idsc->x360.LeftThumbY;
+		int16_t RightThumbX = idsc->x360.RightThumbX;
+		int16_t RightThumbY = idsc->x360.RightThumbY;
+		//bool isPressed = true;
+		//bool isReleased = false;
+		if (Ruyi::SDK::GlobalInputDefine::GamepadButtonFlags::DPadUp == Buttons)
+		{
+			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler x360 DPadUp"));
+		}		
+	}
+	if (idsc->__isset.dgamepad == 1)
+	{
+		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler dgamepad:%d"), idsc->__isset.dgamepad);
+	}
+	if (idsc->__isset.djoystick == 1)
+	{
+		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler djoystick:%d"), idsc->__isset.djoystick);
+	}
+	if (idsc->__isset.dkeyboard == 1)
+	{
+		char key = idsc->dkeyboard.Key;
+		bool isPressed = idsc->dkeyboard.IsPressed;
+		bool isReleased = idsc->dkeyboard.IsReleased;
+
+		if ((Ruyi::SDK::GlobalInputDefine::Key::W == key || Ruyi::SDK::GlobalInputDefine::Key::Up == key) && isPressed)
+		{
+			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler keybaord W"));
+		}
+	}
+	if (idsc->__isset.dmouse == 1)
+	{
+		//UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler dmouse:%d"), idsc->__isset.dmouse);
+	}
+	if (idsc->__isset.ruyicontroller == 1)
+	{
+		int64_t PacketId = idsc->ruyicontroller.PacketId;
+		int32_t ChannelId = idsc->ruyicontroller.ChannelId;
+		//FString fDeviceId = UTF8_TO_TCHAR(idsc->ruyicontroller.DeviceId.c_str());
+		int32_t KeyPress = idsc->ruyicontroller.KeyPress;
+		int8_t AnalogL2 = idsc->ruyicontroller.AnalogL2;
+		int8_t AnalogR2 = idsc->ruyicontroller.AnalogR2;
+		int8_t AnalogLeftJoyX = idsc->ruyicontroller.AnalogLeftJoyX;
+		int8_t AnalogLeftJoyY = idsc->ruyicontroller.AnalogLeftJoyY;
+		int8_t AnalogRightJoyX = idsc->ruyicontroller.AnalogRightJoyX;
+		int8_t AnalogRightJoyY = idsc->ruyicontroller.AnalogRightJoyY;
+		//tempoary, ruyi controller not support isPressd/isReleased
+		bool isPressed = true;
+		bool isReleased = false;
+		if (Ruyi::SDK::GlobalInputDefine::RuyiControllerKey::eButtonUp == KeyPress)
+		{
+			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller eButtonUp"));
+		}
+		
+		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller PacketId:%d, ChannelId:%d"), PacketId, ChannelId);
+		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller KeyPress:%d, AnalogL2:%d, AnalogR2:%d"), KeyPress, AnalogL2, AnalogR2);
+		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller AnalogLeftJoyX:%d, AnalogLeftJoyY:%d, AnalogRightJoyX:%d, AnalogRightJoyY:%d"), AnalogLeftJoyX, AnalogLeftJoyY, AnalogRightJoyX, AnalogRightJoyY);
+	}
 }
 
 // Input
