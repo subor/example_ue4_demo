ecbac2fa535d0b51c7ad22ce6df291581b403ebd
PLAYRUYI\Goden <goden.zheng@playruyi.com>
2018-04-26 12:37:57 +0800

    [JP-1934]Support Ruyi controller in Unreal demo & New Unreal Demo on Bitbucket org, update the input logic with InputActionEvent interface

--- Source/RuyiSDKDemo/RuyiSDKDemoCharacter.cpp
+++ Source/RuyiSDKDemo/RuyiSDKDemoCharacter.cpp
@@ -95,86 +95,52 @@ void ARuyiSDKDemoCharacter::InputStateChangeHandler(std::string topic, apache::t
 	//if if you wanna call the input logic in blueprints but still get the input from Ruyi SDK
 	//We recommand you listener all the input in c++ from Ruyi SDK, then call in blueprints
 
-	//UE_LOG(CommonLog, Log, TEXT("ARuyiSDKDemoCharacter::InputStateChangeHandler !!!"));
+	FString fTopic = UTF8_TO_TCHAR(topic.c_str());
+	//UE_LOG(LogPlatformer, Log, TEXT("InputStateChangeHandler DDDDDDDDDDDDDD topic:%s"), *fTopic);
 
-	auto idsc = dynamic_cast<Ruyi::SDK::InputManager::InputDeviceStateChanged*>(msg);
+	auto idsc = dynamic_cast<Ruyi::SDK::UserServiceExternal::InputActionEvent*>(msg);
 
 	if (idsc == NULL)
 	{
 		return;
 	}
 
-	if (idsc->__isset.header == 1)
-	{
-		//UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler header:%d"), idsc->__isset.header);
-	}
-	if (idsc->__isset.x360 == 1)
-	{
-		int64_t PacketNumber = idsc->x360.PacketNumber;
-		//FString fDeviceId = UTF8_TO_TCHAR(idsc->x360.DeviceId.c_str());
-		int16_t LeftThumbDeadZone = idsc->x360.LeftThumbDeadZone;
-		int16_t RightThumbDeadZone = idsc->x360.RightThumbDeadZone;
-		int32_t Buttons = idsc->x360.Buttons;
-		int8_t LeftTrigger = idsc->x360.LeftTrigger;
-		int8_t RightTrigger = idsc->x360.RightTrigger;
-		int16_t LeftThumbX = idsc->x360.LeftThumbX;
-		int16_t LeftThumbY = idsc->x360.LeftThumbY;
-		int16_t RightThumbX = idsc->x360.RightThumbX;
-		int16_t RightThumbY = idsc->x360.RightThumbY;
-		//bool isPressed = true;
-		//bool isReleased = false;
-		if (Ruyi::SDK::GlobalInputDefine::GamepadButtonFlags::DPadUp == Buttons)
-		{
-			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler x360 DPadUp"));
-		}		
-	}
-	if (idsc->__isset.dgamepad == 1)
-	{
-		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler dgamepad:%d"), idsc->__isset.dgamepad);
-	}
-	if (idsc->__isset.djoystick == 1)
-	{
-		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler djoystick:%d"), idsc->__isset.djoystick);
-	}
-	if (idsc->__isset.dkeyboard == 1)
-	{
-		char key = idsc->dkeyboard.Key;
-		bool isPressed = idsc->dkeyboard.IsPressed;
-		bool isReleased = idsc->dkeyboard.IsReleased;
+	//userId is the id of the current logged account
+	FString fUserId = UTF8_TO_TCHAR(idsc->userId.c_str());
+	FString fAction = UTF8_TO_TCHAR(idsc->action.c_str());
 
-		if ((Ruyi::SDK::GlobalInputDefine::Key::W == key || Ruyi::SDK::GlobalInputDefine::Key::Up == key) && isPressed)
-		{
-			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler keybaord W"));
-		}
-	}
-	if (idsc->__isset.dmouse == 1)
+	//std::vector<TriggerKeys>  Triggers is the numbers of current input from any device
+	//TriggerKeys 
+	//DeviceType: to identify your input device
+	//Key: the key of your input device
+	//NewValue/OldValue:  could be three value:0,1,2.  1 means press Down 2 means release 0 not define yet
+	//NewValue is the current key state, if your press down, NewValue will be 1, when you release, NewValue will be 2, OldValue will be 1
+	int triggerNumber = idsc->Triggers.size();
+
+	UE_LOG(LogPlatformer, Log, TEXT("InputStateChangeHandler userId:%s action:%s triggers Num:%d"), *fUserId, *fAction, triggerNumber);
+	std::for_each(idsc->Triggers.begin(), idsc->Triggers.end(), [&](Ruyi::SDK::UserServiceExternal::TriggerKeys& key)
 	{
-		//UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler dmouse:%d"), idsc->__isset.dmouse);
-	}
-	if (idsc->__isset.ruyicontroller == 1)
-	{
-		int64_t PacketId = idsc->ruyicontroller.PacketId;
-		int32_t ChannelId = idsc->ruyicontroller.ChannelId;
-		//FString fDeviceId = UTF8_TO_TCHAR(idsc->ruyicontroller.DeviceId.c_str());
-		int32_t KeyPress = idsc->ruyicontroller.KeyPress;
-		int8_t AnalogL2 = idsc->ruyicontroller.AnalogL2;
-		int8_t AnalogR2 = idsc->ruyicontroller.AnalogR2;
-		int8_t AnalogLeftJoyX = idsc->ruyicontroller.AnalogLeftJoyX;
-		int8_t AnalogLeftJoyY = idsc->ruyicontroller.AnalogLeftJoyY;
-		int8_t AnalogRightJoyX = idsc->ruyicontroller.AnalogRightJoyX;
-		int8_t AnalogRightJoyY = idsc->ruyicontroller.AnalogRightJoyY;
-		//tempoary, ruyi controller not support isPressd/isReleased
-		bool isPressed = true;
-		bool isReleased = false;
-		if (Ruyi::SDK::GlobalInputDefine::RuyiControllerKey::eButtonUp == KeyPress)
+		//UE_LOG(LogPlatformer, Log, TEXT("InputStateChangeHandler deviceType:%d, key:%d, newValue:%d, oldValue:%d"), key.DeviceType, key.Key, key.NewValue, key.OldValue);
+
+		if (Ruyi::SDK::GlobalInputDefine::RuyiInputDeviceType::Keyboard == key.DeviceType)
 		{
-			UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller eButtonUp"));
-		}
-		
-		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller PacketId:%d, ChannelId:%d"), PacketId, ChannelId);
-		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller KeyPress:%d, AnalogL2:%d, AnalogR2:%d"), KeyPress, AnalogL2, AnalogR2);
-		UE_LOG(CommonLog, Log, TEXT("InputStateChangeHandler ruyicontroller AnalogLeftJoyX:%d, AnalogLeftJoyY:%d, AnalogRightJoyX:%d, AnalogRightJoyY:%d"), AnalogLeftJoyX, AnalogLeftJoyY, AnalogRightJoyX, AnalogRightJoyY);
-	}
+			if ((Ruyi::SDK::GlobalInputDefine::Key::W == key.Key || Ruyi::SDK::GlobalInputDefine::Key::Up == key.Key) && (1 == key.NewValue))
+			{
+				//if you wanna call the input logic in blueprints but still get the input from Ruyi SDK
+				//We recommand you listener all the input in c++ from Ruyi SDK, then call in blueprints
+			}
+		} else if (Ruyi::SDK::GlobalInputDefine::RuyiInputDeviceType::XB360 == key.DeviceType)
+		{
+			if ((Ruyi::SDK::GlobalInputDefine::GamepadButtonFlags::DPadUp == key.Key) && (1 == key.NewValue))
+			{
+			}
+		} else if (Ruyi::SDK::GlobalInputDefine::RuyiInputDeviceType::RuyiController == key.DeviceType)
+		{
+			if (Ruyi::SDK::GlobalInputDefine::RuyiControllerKey::eButtonUp == key.Key && (1 == key.NewValue))
+			{
+			}
+		} else {}
+	});
 }
 
 // Input
